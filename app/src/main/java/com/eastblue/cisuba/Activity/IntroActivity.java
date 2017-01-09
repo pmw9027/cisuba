ed5ce5e52fd762b86fe5757d9061325c0dba6d73
SlZeroth <qkrwncks593@gmail.com>
2017-01-09 13:47:53 +0900

    드로어 레이아웃 추가

--- app/src/main/java/com/eastblue/cisuba/Activity/IntroActivity.java
+++ app/src/main/java/com/eastblue/cisuba/Activity/IntroActivity.java
@@ -1,5 +1,7 @@
 package com.eastblue.cisuba.Activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,19 @@ import com.eastblue.cisuba.R;
 
 public class IntroActivity extends AppCompatActivity {
 
+    private static final int DELAY = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, DELAY);
     }
 }
--- app/src/main/java/com/eastblue/cisuba/Activity/MainActivity.java
+++ app/src/main/java/com/eastblue/cisuba/Activity/MainActivity.java
@@ -2,14 +2,34 @@ package com.eastblue.cisuba.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.eastblue.cisuba.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
+    }
+
+    void init() {
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
