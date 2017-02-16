bdc34a4644ae50e06f19f2e38081eeab9f8326ca
DESKTOP-GI2SL1K\PJC <qkrwncks593@gmail.com>
2017-02-16 17:39:07 +0900

    공지사항 FAQ 레이아웃 추가

---
+++ app/src/main/java/com/eastblue/cisuba/Activity/NoticeActivity.java
@@ -0,0 +1,42 @@
+package com.eastblue.cisuba.Activity;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+
+import com.eastblue.cisuba.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.btn_notice) Button btnNotice;
+    @BindView(R.id.btn_faq) Button btnFaq;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @OnClick(R.id.btn_notice)
+    void onNotice() {
+        btnFaq.setTextColor(Color.BLACK);
+        btnNotice.setTextColor(getResources().getColor(R.color.product_color));
+    }
+
+    @OnClick(R.id.btn_faq)
+    void onFaq() {
+        btnNotice.setTextColor(Color.BLACK);
+        btnFaq.setTextColor(getResources().getColor(R.color.product_color));
+    }
+}
--- app/src/main/java/com/eastblue/cisuba/Fragment/ProfileFragment.java
+++ app/src/main/java/com/eastblue/cisuba/Fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.eastblue.cisuba.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,9 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.eastblue.cisuba.Activity.NoticeActivity;
+import com.eastblue.cisuba.Activity.RequestPartnerActivity;
 import com.eastblue.cisuba.R;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by PJC on 2017-02-07.
@@ -23,5 +27,20 @@ public class ProfileFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this, rootView);
         return rootView;
+    }
+
+    @OnClick(R.id.btn_request_partner)
+    void requestPartner() {
+        startActivity(new Intent(getActivity(), RequestPartnerActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    @OnClick(R.id.lin_notice)
+    void openNotice() {
+        startActivity(new Intent(getActivity(), NoticeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    @OnClick(R.id.lin_faq)
+    void openFaq() {
+        startActivity(new Intent(getActivity(), NoticeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
     }
 }
